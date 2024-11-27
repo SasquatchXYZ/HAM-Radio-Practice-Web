033bad8bc30dcf1cee3dfbf70c596208b8664da2
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-11-27 12:38:58 -0500

    Add stubs for the classes

---
+++ app/models/questions.py
@@ -0,0 +1,12 @@
+class Questions:
+    def connect_to_database(self):
+        pass
+
+    def update_database(self):
+        pass
+
+    def validate_and_cleanse_data(self):
+        pass
+
+    def fetch_questions(self):
+        pass
---
+++ app/utils/grader.py
@@ -0,0 +1,9 @@
+class Grader:
+    def compare_answers(self):
+        pass
+
+    def calculate_final_score(self):
+        pass
+
+    def hand_unanswered_questions(self):
+        pass
