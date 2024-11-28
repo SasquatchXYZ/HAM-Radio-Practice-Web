5d435cdeb322aadf7f6073cc77bf70047506ba43
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-11-28 13:22:08 -0500

    Cleanup questions.py

--- app/models/questions.py
+++ app/models/questions.py
@@ -1,6 +1,3 @@
-import sqlite3
-
-
 class Questions:
     def __init__(self, cursor):
         self.cursor = cursor
@@ -8,15 +5,3 @@ class Questions:
     def fetch_data(self):
         self.cursor.execute("SELECT * FROM questions")  # Adjust SQL query as needed
         return self.cursor.fetchall()
-
-    def update_database(self):
-        # TODO: Implement database update logic
-        pass
-
-    def validate_and_cleanse_data(self):
-        # TODO: Implement data validation and cleansing logic
-        pass
-
-    def fetch_questions(self):
-        # TODO: Implement logic to fetch questions from the database
-        pass
