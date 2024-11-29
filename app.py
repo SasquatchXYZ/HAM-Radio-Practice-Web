69538292279a4bed4db62ab1db39b4f5a520199a
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-11-28 22:03:39 -0500

    Updating the UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from app.models.database_connection import DatabaseConnection
 from app.models.questions import Questions
 from app.models.session import Session
@@ -6,15 +6,21 @@ from app.models.session import Session
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    db_path = 'data/questions.db'
-    # Call the fucntion and store the returned data in a variable
-    with DatabaseConnection(db_path) as cursor:
-        session = Session(cursor)
-        print("Session ID:", session.session_id)
-        questions = Questions(cursor)
-        data = questions.get_question_set()
+    # Check if the form has been submitted
+    if request.method == 'POST':
+        # Start the question session
+        # ...
+        db_path = 'data/questions.db'
+        # Call the fucntion and store the returned data in a variable
+        with DatabaseConnection(db_path) as cursor:
+            session = Session(cursor)
+            print("Session ID:", session.session_id)
+            questions = Questions(cursor)
+            data = questions.get_question_set()
+    else:
+        data = None
     # Return the HTML template for the index page
     return render_template('index.html', data=data)
 
