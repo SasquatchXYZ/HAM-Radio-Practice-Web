1479f5e0606018c822586f4ec64dd3bebde3b6fc
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-11-28 22:40:58 -0500

    Saving SessionId as a cookie

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 from app.models.database_connection import DatabaseConnection
 from app.models.questions import Questions
 from app.models.session import Session
@@ -8,21 +8,25 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    # Check if the form has been submitted
-    if request.method == 'POST':
-        # Start the question session
-        # ...
-        db_path = 'data/questions.db'
-        # Call the fucntion and store the returned data in a variable
-        with DatabaseConnection(db_path) as cursor:
-            session = Session(cursor)
-            print("Session ID:", session.session_id)
+    db_path = 'data/questions.db'
+    # Call the function and store the returned data in a variable
+    with DatabaseConnection(db_path) as cursor:
+        session = Session(cursor)
+        current_session = request.cookies.get('session_id')
+        if current_session is None:
             questions = Questions(cursor)
             data = questions.get_question_set()
-    else:
-        data = None
-    # Return the HTML template for the index page
-    return render_template('index.html', data=data)
+            session.session_id = session.create_session()
+            response = make_response(render_template('index.html', data=data, session_id=session.session_id))
+            response.set_cookie('session_id', str(session.session_id))
+            return response
+        else:
+            # We have a cookie value and existing id
+            session.session_id = current_session
+            questions = Questions(cursor)
+            data = questions.get_question_set()
+            response = make_response(render_template('index.html', data=data, session_id=session.session_id))
+            return response
 
 
 if __name__ == '__main__':
