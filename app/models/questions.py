7ca6133d81a95676a1ba491c1a2c0354be951455
Dalton Ricker <40616938+SasquatchXYZ@users.noreply.github.com>
2024-11-29 13:33:16 -0500

    Update questions.py

--- app/models/questions.py
+++ app/models/questions.py
@@ -9,6 +9,25 @@ class Questions:
         self.cursor.execute("SELECT id FROM questions")  # Adjust SQL query as needed
         return self.cursor.fetchall()
 
+    def get_question(self, question_id):
+        self.cursor.execute('''SELECT id, correct, question, a, b, c, d FROM questions WHERE id = ?''', (question_id,))
+        result = self.cursor.fetchone()
+
+        if result is None:
+            return None
+
+        question_dict = {
+            'id': result[0],
+            'correct': result[1],
+            'question': result[2],
+            'a': result[3],
+            'b': result[4],
+            'c': result[5],
+            'd': result[6]
+        }
+
+        return question_dict
+
     def get_question_set(self, session_id=None):
         if session_id is None:
             all_questions = self.get_all_questions()
@@ -43,3 +62,61 @@ class Questions:
                 return None
             else:
                 return list(result)
+
+    def get_answered_questions(self, session_id):
+        self.cursor.execute("SELECT questions_correct, questions_incorrect FROM sessions WHERE session_id = ?",
+                            (session_id,))
+        result = self.cursor.fetchone()
+        questions_correct, questions_incorrect = result if result else (0, 0)
+
+        # Calculate the total number of questions answered
+        questions_answered = questions_correct + questions_incorrect
+        return int(questions_answered)
+
+    def get_next_question(self, session_id):
+        # Execute the first query and fetch all results
+        self.cursor.execute("SELECT question_id FROM question_sets WHERE session_id = ?", (session_id,))
+        question_ids = [row[0] for row in self.cursor.fetchall()]
+
+        print(question_ids[0])
+
+        questions_answered = self.get_answered_questions(session_id)
+
+        # If questions answered is less than 35, return the question
+        if questions_answered < 35:
+            # print(str(question_id_dict[0]))
+            next_question = str(question_ids[questions_answered])
+            return next_question
+
+        # return next_question
+
+    def store_answer(self, question_id, selected_answer, session_id):
+
+        self.cursor.execute("SELECT correect FROM questions WHERE id = ?", (question_id,))
+
+        result = self.cursor.fetchone()
+        correct_answer = int(result[0])
+
+        print("Correct answer is " + str(result[0]) + " and you selected " + str(selected_answer))
+
+        if correct_answer == int(selected_answer):
+            print("Correct")
+            self.cursor.execute("UPDATE sessions SET questions_correct = questions_correct + 1 WHERE session_id = ?",
+                                (session_id,))
+        else:
+            print("Incorrect")
+            self.cursor.execute(
+                "UPDATE sessions SET questions_incorrect = questions_incorrect + 1 WHERE session_id = ?",
+                (session_id,))
+        return "test"
+
+    def mark_answer_wrong(self, session_id):
+        self.cursor.execute("UPDATE sessions SET questions_incorrect = questions_incorrect + 1 WHERE session_id = ?",
+                            (session_id,))
+
+    def tally_results(self, session_id):
+        self.cursor.execute("SELECT questions_correct, questions_incorrect FROM sessions WHERE session_id = ?",
+                            (session_id,))
+        result = self.cursor.fetchone()
+        questions_correct, questions_incorrect = result if result else (0, 0)
+        return {"questions_correct": questions_correct, "questions_incorrect": questions_incorrect}
